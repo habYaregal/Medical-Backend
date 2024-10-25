e344bffc1da2a68daa57f59188b184c63a7208f4
habYaregal <habtemariamyaregal@gmail.com>
2024-10-25 20:57:44 +0300

    adding testing

--- DbConfigs/DbConfig.js
+++ DbConfigs/DbConfig.js
@@ -6,5 +6,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     dialect: process.env.DB_DIALECT
 });
 
-
+sequelize.authenticate()
+  .then(() => console.log('Database connected successfully!'))
+  .catch(err => console.error('Unable to connect to the database:', err));
 export default sequelize;
